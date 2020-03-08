1dab62acf20f1e0fe6e1a8e89386c74a1824c122
alexeideriglzov <alexeideriglazov@gmail.com>
2020-03-08 22:12:59 +0300

    refactoring

--- src/test/java/helpers/Constants.java
+++
@@ -1,5 +0,0 @@
-package helpers;
-
-public abstract class Constants {
-    public static final double EPS = 1e-15;
-}
--- src/test/java/helpers/file/Operations.java
+++
@@ -1,8 +0,0 @@
-package helpers.file;
-
-public enum Operations {
-    sum,
-    diff,
-    div,
-    mult
-}
